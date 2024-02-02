34bfde15080d5128d6a51e4ed023fc4434ac60ab
jarne.juechser <jarne.juechser@app-akademie.com>
2024-02-02 15:48:57 +0100

    added comments

--- lib/User.dart
+++ lib/User.dart
@@ -1,13 +1,14 @@
 import 'package:hive_flutter/hive_flutter.dart';
-
+//wird gebraucht, damit der generator erkennt wovon der Adapter erzeugt werden soll
 part 'User.g.dart';
-
+//type id sollte unique sein
 @HiveType(typeId: 0)
 class User{
+  // Hive field nummern sollten sich nicht doppeln
   @HiveField(0)
   String name;
   @HiveField(1)
   String email;
-
+  // Functionen müssen wir nicht kennzeichnen
   User({required this.name, required this.email});
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive_workshop_batch2/User.dart';
 
 void main() async{
+  // Damit initialisieren wir Hive vor dem Starten der App
   await Hive.initFlutter();
+  // Wir öffnen eine Box in die wir alle primitiven Datentypen speichern können
   var box = await Hive.openBox('testBox');
-
+  // registriert den Adapter der es uns erlaubt User(also eigene Klassen) in Boxen zu speichern
   Hive.registerAdapter(UserAdapter());
-
+  // für Boxen mit Listen oder Maps müssen wir den Datentyp angeben(beim öffnen/schließen/aufrufen)
   await Hive.openBox<List>('users');
 
   runApp(MainApp());
@@ -18,7 +20,7 @@ class MainApp extends StatelessWidget {
   MainApp({super.key});
   TextEditingController nameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
-
+  // Liest die liste mit Usern aus der List Box aus
   List users = Hive.box<List>('users').get('personen') ?? [];
 
   @override
@@ -37,13 +39,18 @@ class MainApp extends StatelessWidget {
               controller: emailController,
             ),
             ElevatedButton(onPressed: (){
+              //speichert den Namen des Textfelds in der simplen Box
               Hive.box('testBox').put("name", nameController.text);
+              // erstellt einen User aus den Textfield inputs
               User user = User(name: nameController.text, email: emailController.text);
+              // fügt den user zur bestehenden Liste hinzu
               users.add(user);
+              // läd die Liste mit dem neuen User in die List box
               Hive.box<List>('users').put('personen',users);
             }, child: Text("save name to box")),
             Text(Hive.box('testBox').get('name') ?? "no name saved"),
             Expanded(
+              // ZEigt die Liste an Usern an
               child: ListView.builder(
                 itemCount: users.length,
                 itemBuilder: (context,index){
